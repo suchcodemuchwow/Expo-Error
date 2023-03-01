119ce2540520c07beb9b41f02c872480fe115b2d
Cavit Baturalp Gürdin <cavit.gurdin@gmail.com>
2023-03-01 18:07:49 +0100

    first commit

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <FontAwesome name="home" size={20} />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
